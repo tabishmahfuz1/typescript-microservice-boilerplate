e478291b58b32f1f80657b4f6d2995f1bf12f571
tabish <tabish@catapultprime.com>
2020-11-30 15:03:17 +0530

    Added Timestamp in Logger
    Change-Id: I15910e16eb9fc6f3d0eb3f4ae7dcded4cbf319c2

--- src/app/Log.ts
+++ src/app/Log.ts
@@ -61,21 +61,21 @@ export class Log4jsLogger implements Logger {
 
 @injectable()
 export class STDOutLogger implements Logger {
-    
     constructor(@inject(TYPES.Config) private config: Config) {
-        console.log("ENV is", this.config.get("nodeENV"))
+        console.log("ENV is", this.config.get("nodeENV"));
     }
 
     info(str: string, data?: any) {
-        console.log(str, data);
+        console.log(`[${new Date().toISOString()}][INFO]`, str, data || '');
     }
 
     debug(str: string, data?: any) {
-        this.config.get("nodeENV") === "development" && console.log(str, data);
+        this.config.get("nodeENV") !== "production" && 
+        console.log(`[${new Date().toISOString()}][DEBUG]`, str, data || '');
     }
 
     error(str: string, data?: any) {
-        console.error(str, data);
+        console.error(`[${new Date().toISOString()}][ERROR]`, str, data || '');
     }
 }
 
